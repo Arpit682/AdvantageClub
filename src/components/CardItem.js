60b55c994117435ce04ae3cb07710a3abf918ec0
ARPIT GOYAL <arpitg@cvent.com>
2020-05-19 05:40:28 +0530

    Initial setup

---
+++ src/components/CardItem.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
+export class CardItem  extends React.Component {
+    render() {
+        return (
+            <Card>
+                <CardHeader
+                    title={this.props.title}
+                    subheader={this.props.address}
+                />
+                <CardMedia
+                    image="/static/images/cards/paella.jpg"
+                    title="Paella dish"
+                />
+                <CardContent>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        Buy Now
+                    </Typography>
+                </CardContent>
+            </Card>
+        );
+    }
+}
